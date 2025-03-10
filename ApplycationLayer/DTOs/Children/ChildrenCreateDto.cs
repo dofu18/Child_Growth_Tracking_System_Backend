1ad10d4a00759e68acc530814679665b4c938c93
dofu18 <phu2003hk@gmail.com>
2025-03-11 02:47:37 +0700

    register doctor

--- ApplycationLayer/DTOs/Children/ChildrenCreateDto.cs
+++ ApplycationLayer/DTOs/Children/ChildrenCreateDto.cs
@@ -17,6 +17,6 @@ namespace ApplicationLayer.DTOs.Childrens
         public GenderEnum Gender { get; set; }
         public decimal Weight { get; set; }
         public decimal Height { get; set; }
-        public string Notes { get; set; }
+        public string? Notes { get; set; }
     }
 }
