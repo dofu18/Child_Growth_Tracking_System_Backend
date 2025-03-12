15ee56881a6451bb057807192669c4296a3cb250
dofu18 <phu2003hk@gmail.com>
2025-03-13 00:51:27 +0700

    migrate db

---
+++ ApplycationLayer/DTOs/Consultation/ConsultationRequests/ConsultationRequestDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationLayer.DTOs.Children;
+using DomainLayer.Entities;
+using static DomainLayer.Enum.GeneralEnum;
+
+namespace ApplicationLayer.DTOs.Consultation.ConsultationRequests
+{
+    public class ConsultationRequestDto
+    {
+        public Guid ChildrentId { get; set; }
+        public DateTime RequestDate { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public ConsultationRequestStatusEnum? Status { get; set; }
+        public string Attachments { get; set; }
+        public Guid UserRequestId { get; set; }
+        public Guid DoctorReceiveId { get; set; }
+        public User DoctorReceive { get; set; }
+        public User UserRequest { get; set; }
+        public ChildrentResponseDto Children { get; set; }
+    }
+}
