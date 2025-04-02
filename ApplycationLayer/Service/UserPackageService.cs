91752bf51fb1276c366daa8bc927e8c2fb9cafcc
dofu18 <phu2003hk@gmail.com>
2025-04-02 18:33:38 +0700

    check package duration

--- ApplycationLayer/Service/UserPackageService.cs
+++ ApplycationLayer/Service/UserPackageService.cs
@@ -28,6 +28,8 @@ namespace ApplicationLayer.Service
         //admin
         Task<IActionResult> GetNumberUsingPackage();
         Task<IActionResult> UpdatePackageStatus(Guid packageId, [FromBody] PackageStatusEnum newStatus);
+        //check duration
+        Task<IActionResult> CheckDuration();
     }
     public class UserPackageService : BaseService, IUserPackageService
     {
@@ -317,7 +319,7 @@ namespace ApplicationLayer.Service
 
             try
             {
-                var userPackages = await _userPackageRepo.WhereAsync(up => up.OwnerId == userId, "Package");
+                var userPackages = await _userPackageRepo.WhereAsync(up => up.OwnerId == userId && up.Status == UserPackageStatusEnum.OnGoing, "Package");
 
                 if (!userPackages.Any())
                 {
@@ -402,6 +404,37 @@ namespace ApplicationLayer.Service
                 PackageId = package.Id,
                 NewStatus = newStatus
             });
+        }
+
+        public async Task<IActionResult> CheckDuration()
+        {
+            var payload = ExtractPayload();
+            if (payload == null)
+            {
+                return ErrorResp.Unauthorized("Invalid token");
+            }
+
+            var userId = payload.UserId;
+            var onGoingPack = await _userPackageRepo.FirstOrDefaultAsync(up => up.OwnerId == userId && up.Status == UserPackageStatusEnum.OnGoing);
+            var today = DateOnly.FromDateTime(DateTime.UtcNow);
+
+            if (onGoingPack == null)
+            {
+                return SuccessResp.Ok("No package");
+            }
+
+            if (onGoingPack.ExpireDate >= today)
+            {
+                onGoingPack.Status = UserPackageStatusEnum.OnGoing;
+            }
+            else
+            {
+                onGoingPack.Status = UserPackageStatusEnum.Expired;
+                onGoingPack.UpdatedAt = DateTime.UtcNow;
+            }
+
+            await _userPackageRepo.UpdateAsync(onGoingPack);
+            return SuccessResp.Ok("Checked package duration");
         }
     }
 }
